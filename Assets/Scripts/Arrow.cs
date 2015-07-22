d197787d9bc223bf4d7d95767a229ccacc5c0577
UltimateRed <redgamer34@gmail.com>
2015-07-21 19:06:00 -0600

    Alpha 1.1.5.9
    added Arrow and ArrowTrap Scripts

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+	
+	public float arrowSpeed = 2f;
+	
+
+	void FixedUpdate () {
+        transform.Translate( -transform.up * arrowSpeed);
+		
+        }
+}
---
+++ Assets/Scripts/ArrowTrap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowTrap : MonoBehaviour {
+    public GameObject Astroid;
+    public float arrowSpawnRate = .5f;
+    public float MinX = -3;
+    public float MaxX = 3;
+    public float MinY = 5;
+    public float MaxY = 6;
+
+    //for 3d you have z position
+    public float MinZ = 0;
+    public float MaxZ = 10;
+
+    //turn off or on 3D placement
+    public bool is3D = false;
+    
+    
+    
+    void Start() {
+
+        InvokeRepeating("Spawn", 2, arrowSpawnRate);
+    }
+
+    void Spawn() {
+        float x = Random.Range(MinX, MaxX);
+        GameObject arrow = (GameObject)Instantiate(Astroid, new Vector3(x, 0, 0) + transform.position, Quaternion.identity);
+		arrow.transform.parent = transform;
+		Destroy(arrow,3f);
+        
+    }
+    
+ 
+    
+}
